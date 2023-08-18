cb4d8adbc93fe550dab0f1e0e6450d569674a679
Utso-Sarkar <devutso428@gmail.com>
2023-08-19 00:47:41 +0600

    added the balance part

--- diposit.js
+++ diposit.js
@@ -1,14 +1,29 @@
 // added event dandaler 1
 document.getElementById('btn-deposit').addEventListener('click',function(){
 const depositField =document.getElementById('deposit-field');
-const depositFix =depositField.value ;
+const depositFixString =depositField.value ;
+const depositFix =parseFloat(depositFixString);
+
 
 
 
 // use the current deposit total ............
 
 const depositTotal =document.getElementById('DepositTotal');
-    const depositElement = depositTotal.innerText;
-    depositTotal.innerText=depositFix;
+    const depositElementString = depositTotal.innerText;
+    const depositElement =parseFloat(depositElementString);
+  
+
+    const currentDepositTotal = depositElement+depositFix;
+    depositTotal.innerText=currentDepositTotal;
+
+// get balance total value
+const balanceTotalElement =document.getElementById('balanceTotal');
+const totalBalanceTotalString =balanceTotalElement.innerText;
+const balance=parseFloat(totalBalanceTotalString);
+const currentBalanceTotal =balance+currentDepositTotal;
+balanceTotalElement.innerText=currentBalanceTotal;
+    // another operation 
+    depositField.value="";
 })
 
